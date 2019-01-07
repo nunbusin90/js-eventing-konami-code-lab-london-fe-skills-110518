8b7856169b23ae0f0f1d55b37484eb3ef84a5135
NA YEON LEE <lee.nayeon90@gmail.com>
2019-01-07 19:22:21 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,15 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+// function init() {
+//   // Write your JavaScript code inside the init() function
+// }
+
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let cursor = 0;
 function init() {
-  // Write your JavaScript code inside the init() function
-
+document.addEventListener('keydown', function(e) {
+   	cursor = (e.keyCode == code[cursor]) ? cursor + 1 : 0;
+if (cursor == code.length) alert("Yes!");
+   });
 }
+init();
